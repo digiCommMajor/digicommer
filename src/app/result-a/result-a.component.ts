5438f1bb8f92475040011544b1f2281fe1ec6f0c
carlybrooks <74929218+carlybrooks@users.noreply.github.com>
2020-12-13 13:46:08 -0500

    Create result-a.component.ts

---
+++ src/app/result-a/result-a.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-result-a',
+  templateUrl: './result-a.component.html',
+  styleUrls: ['./result-a.component.scss']
+})
+export class ResultAComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
