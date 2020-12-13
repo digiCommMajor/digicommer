57449efb3fea9b07168ec24bdd9ef11b174d1465
abigailleon99 <58637906+abigailleon99@users.noreply.github.com>
2020-12-13 15:37:05 -0500

    Added more questions, added results in the console

--- src/app/survey-page/survey-page.component.ts
+++ src/app/survey-page/survey-page.component.ts
@@ -18,11 +18,34 @@ export class SurveyPageComponent implements OnInit {
     this.result.emit(change.value);
   }
 
-  Cardresults: number[] = [0, 0, 0, 0]
+  Cardresults: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
   gotResult = (answer: number, cardNum: number): void => {
     this.Cardresults[cardNum] = answer;
     console.log(this.Cardresults);
+    let total = this.Cardresults[0]
+      + this.Cardresults[1]
+      + this.Cardresults[2]
+      + this.Cardresults[3]
+      + this.Cardresults[4]
+      + this.Cardresults[5]
+      + this.Cardresults[6]
+      + this.Cardresults[7]
+      + this.Cardresults[8]
+      + this.Cardresults[9];
+
+    console.log(total);
+
+    // for (i = 0; i < 10; i++) {
+    //   total = this.Cardresults[i] + this.Cardresults[prev_num];
+    //   console.log(total);
+    //   console.log(this.Cardresults[i]);
+    // }
+
+  }
+
+  addResult = () => {
+
   }
 
   constructor() { }
