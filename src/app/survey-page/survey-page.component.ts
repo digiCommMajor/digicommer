dec74a7c4bcdc0f2e99bfd5bc537ea0fdccc5207
abigailleon99 <58637906+abigailleon99@users.noreply.github.com>
2020-12-15 15:47:15 -0500

    Completed Survey Page Routing
    Now based on the totals we get routed to the correct page!!

--- src/app/survey-page/survey-page.component.ts
+++ src/app/survey-page/survey-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { Router } from '@angular/router'; // import router from angular router
 
 @Component({
   selector: 'app-survey-page',
@@ -12,6 +13,7 @@ export class SurveyPageComponent implements OnInit {
   @Input() maxValue: number;
   @Input() units?: string = '';
   @Input() title: string = '';
+  @Input() total: number;
 
   @Output() result = new EventEmitter<number>();
 
@@ -21,10 +23,11 @@ export class SurveyPageComponent implements OnInit {
 
   Cardresults: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-  gotResult = (answer: number, cardNum: number): void => {
+  /*   goResult() - takes in two paramaters: answer and cardNum of type number
+     -- returns the added total of the user as a number */
+  gotResult = (answer: number, cardNum: number) => {
     this.Cardresults[cardNum] = answer;
-    console.log(this.Cardresults);
-    let total = this.Cardresults[0]
+    this.total = this.Cardresults[0]
       + this.Cardresults[1]
       + this.Cardresults[2]
       + this.Cardresults[3]
@@ -34,22 +37,27 @@ export class SurveyPageComponent implements OnInit {
       + this.Cardresults[7]
       + this.Cardresults[8]
       + this.Cardresults[9];
-
-    console.log(total);
-
-    // for (i = 0; i < 10; i++) {
-    //   total = this.Cardresults[i] + this.Cardresults[prev_num];
-    //   console.log(total);
-    //   console.log(this.Cardresults[i]);
-    // }
-
+    // console.log(this.total); // prints the added total to the console
+    return this.total;
   }
 
-  addResult = () => {
-
+  /*   onSumbit() - when the person is done with the survey they will click submit
+     -- depending on what their total is they will be redirected to another page  */
+  onSubmit = () => {
+    console.log(this.total);
+    if (this.total <= 30) {
+      //  Digi Comm might not be for them so route to result-c
+      this.route.navigate(['/result-c']);
+    } else if (this.total >= 60) {
+      //  Digi Comm might looks like a good fit for them so route to result-a
+      this.route.navigate(['/result-a']);
+    } else {
+      //  Digi Comm is a possibility them so route to result-b
+      this.route.navigate(['/result-b']);
+    }
   }
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   ngOnInit(): void {
   }
